c1d73e29cdd1a4816f4ae979b62caf1d9ddfe029
fathimafida47 <fathimafida8281@gmail.com>
2023-10-26 11:12:43 +0530

    pending seller

--- customer/models.py
+++ customer/models.py
@@ -14,7 +14,26 @@ class Customer(models.Model):
         db_table = 'customer_tb'
     
    
+class Seller(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 20)
+    email = models.CharField(max_length = 30)
+    gender = models.CharField(max_length = 10)
+    city = models.CharField(max_length = 20)
+    country = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 20)
+    company_name =models.CharField(max_length = 20)
+    bank_name = models.CharField(max_length = 20)
+    bank_branch = models.CharField(max_length = 20)
+    IFSC = models.CharField(max_length = 20)
+    picture = models.ImageField(upload_to = 'seller/')
+    account_number = models.CharField(max_length = 20)
+    loginid = models.CharField(max_length = 20)
+    status = models.CharField(max_length = 50,default='pending')
 
+
+    class Meta:
+        db_table = 'seller_tb'
 
 
 
--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from random import randint
+from django.conf import settings
+from django.core.mail import send_mail
 
-from customer.models import Customer
+from customer.models import Customer, Seller
 # Create your views here.
 
 
@@ -33,15 +36,68 @@ def dashboard(request):
 
 
 def seller_register(request):
-    return render(request, 'customer/seller_register.html')
-
+    msg = ''
+    status = False
+    if request.method == 'POST':
+        f_name = request.POST['fname']
+        l_name = request.POST['lname']
+        email = request.POST['email']
+        gender = request.POST['gender']
+        city = request.POST['city']
+        country = request.POST['country']
+        company_name = request.POST['comp_name']
+        bank_name = request.POST['bankname']
+        bank_branch = request.POST['branch']
+        account_number = request.POST['acct_number']
+        ifsc = request.POST['ifsc']
+        seller_image = request.FILES['picture']
+       
+        seller_exist = Seller.objects.filter(email = email).exists()
+        if not seller_exist :
+            seller = Seller(
+                first_name = f_name,
+                last_name = l_name,
+                email = email,
+                gender = gender,
+                city = city,
+                country = country,
+                company_name = company_name,
+                bank_name = bank_name,
+                bank_branch = bank_branch,
+                IFSC = ifsc,
+                picture = seller_image,
+                account_number = account_number
+            )
+            
+            seller.save()
+            msg = 'Account created successfully'
+            status = True
+        else:
+            msg = 'Account already exists'
+            
+    return render(request, 'customer/seller_register.html',{'message':msg})
+    
 
 def seller_login(request):
-    return render(request, 'customer/seller_login.html')
+    msg = ''
+    if request.method == 'POST':
+        s_username = request.POST['seller_id']
+        s_password = request.POST['password']
+
+        seller = Seller.objects.filter(loginid = s_username, password = s_password)
+
+        if seller.exists():
+                request.session['seller'] = seller[0].id
+                return redirect('Seller:seller_home')
+        else: 
+                msg = 'Incorrect password/Id'
+
+    return render(request, 'customer/seller_login.html',{'message' : msg})
 
 
 def customer_signup(request):
     msg = ''
+    status = False
     if request.method == 'POST':
         f_name = request.POST['fname']
         l_name = request.POST['lastname']
@@ -50,24 +106,40 @@ def customer_signup(request):
         city = request.POST['city']
         country = request.POST['country']
         password = request.POST['password']
+
+        customer_exist = Customer.objects.filter(email = email).exists()
         
-        customer = Customer(
-            first_name = f_name,
-            last_name = l_name,
-            email = email, 
-            gender = gender,
-            city = city,
-            country = country, 
-            password = password
-            )
-        
-        customer.save()
-        msg = 'Registration successfull'
-    return render(request, 'customer/customer_signup.html', {'message':msg})
+        if not customer_exist:
+            customer = Customer(
+                first_name = f_name,
+                last_name = l_name,
+                email = email, 
+                gender = gender,
+                city = city,
+                country = country, 
+                password = password)
+            customer.save()
+            msg = 'Registration successfull'
+            status = True
+        else:
+            msg = 'Already registered'
+    return render(request, 'customer/customer_signup.html', {'message': msg,'status': status})
 
 
 def customer_login(request):
-    return render(request, 'customer/customer_login.html')
+    msg = ''
+    if request.method == 'POST':
+        c_email = request.POST['email']
+        c_password = request.POST['password']
+
+        customer = Customer.objects.filter(email = c_email, password = c_password)
+
+        if customer.exists():
+            request.session['customer'] = customer[0].id
+            return redirect('customer:customer_home')
+        else: 
+            msg = 'Incorrect password/Username'
+    return render(request, 'customer/customer_login.html',{'message' : msg})
 
 
 def forgot_password_customer(request):
